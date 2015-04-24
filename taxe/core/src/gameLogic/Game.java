8616840a5df03029128e86bab3d977f53d98cfe3
chrisbucklenufc <chrisbucklenufc@gmail.com>
2015-04-24 20:27:40 +0100

    More javadocs

--- taxe/core/src/gameLogic/Game.java
+++ taxe/core/src/gameLogic/Game.java
@@ -28,26 +28,51 @@ import adb.taxe.record.SaveManager;
 
 import com.badlogic.gdx.math.MathUtils;
 
+/**Main Game class of the Game. Handles all of the game logic.*/
 public class Game{
 	public static final boolean trongEnabled = true;
 	//This is sort of a super-class that can be accessed throughout the system as many of its methods are static
 	//This is a useful tool to exploit to make implementing certain features easier
+	/**The instance that the game is running in.*/
 	private static Game instance;
+
+	/**The game's PlayerManager that handles both of the players.*/
 	private PlayerManager playerManager;
+
+	/**The game's GoalManager that handles goals for the players.*/
 	private GoalManager goalManager;
+
+	/**The game's ResourceManager that handles resources for the players.*/
 	private ResourceManager resourceManager;
+
+	/**The game's ObstacleManager that handles Obstacles in the game.*/
 	private ObstacleManager obstacleManager;
+
+	/**The game map for this instance.*/
 	private Map map;
+
+	/**The game state.*/
 	private GameState state;
+
+	/**List of listeners that listen to changes in game state.*/
 	private List<GameStateListener> gameStateListeners = new ArrayList<GameStateListener>();
+
+	/**List of listeners that listen to changes in obstacles.*/
 	private List<ObstacleListener> obstacleListeners = new ArrayList<ObstacleListener>();
+
+	/** Mode saying whether the game is finished by number of turns or number of points */
 	private String MODE;
+
 	private static String previousTurn = "";
 	private static String nextPreviousTurn = "";
 	//Default values
+	/** The number of turns until the game is finished */
 	public int TOTAL_TURNS = 30;
+
+	/**The score a player must reach to win the game.*/
 	public int MAX_POINTS = 3000;
 
+	/**The Instantiation method, sets up the players and game listeners.*/
 	private Game(String p1Name, String p2Name, String MODE, int count) {
 		this.MODE = MODE;
 		if(MODE.equals(GameSetupScreen.MODETURNS))
@@ -70,10 +95,10 @@ public class Game{
 
 		map = new Map(false, null);
 		obstacleManager = new ObstacleManager(map);
-		
+
 
 		state = GameState.NORMAL;
-		
+
 		//Adds all the subscriptions to the game which gives players resources and goals at the start of each turn.
 		//Also decrements all connections and blocks a random one
 		//The checking for whether a turn is being skipped is handled inside the methods, this just always calls them
@@ -93,7 +118,7 @@ public class Game{
 		});
 	}
 
-	//Constructor to be used when loading a game from a file
+	/** Constructor to be used when loading a game from a file */
 	public Game(String MODE, int totalTurns, int maxPoints, PlayerManager pm, Map m, ObstacleManager om, final boolean isRecording) {
 		this.MODE = MODE;
 		TOTAL_TURNS = totalTurns;
@@ -105,10 +130,10 @@ public class Game{
 
 		map = m;
 		obstacleManager = om;
-		
+
 
 		state = GameState.NORMAL;
-		
+
 		//Adds all the subscriptions to the game which gives players resources and goals at the start of each turn.
 		//Also decrements all connections and blocks a random one
 		//The checking for whether a turn is being skipped is handled inside the methods, this just always calls them
@@ -127,12 +152,13 @@ public class Game{
 			}
 		});
 	}
-	
+
 	public void save()
 	{
 		SaveManager.save();
 	}
 
+	/**Returns the main game instance.*/
 	public static Game getInstance(String p1Name, String p2Name, String MODE, int count) {
 		if (instance == null) {
 			instance = new Game(p1Name, p2Name, MODE, count);
@@ -143,15 +169,14 @@ public class Game{
 		return instance;
 	}
 
-	// Only the first player should be given goals and resources during init
-	// The second player gets them when turn changes!
+	/**Sets up the players. Only the first player is given goals and resources initially.*/
 	private void initialisePlayers() {
 		Player player = playerManager.getAllPlayers().get(0);
 		goalManager.addRandomGoalToPlayer(player, true);
 		resourceManager.addRandomResourceToPlayer(player, true);
 		resourceManager.addRandomResourceToPlayer(player, true);
 	}
-	
+
 	public String getMode()
 	{
 		return MODE;
@@ -177,6 +202,7 @@ public class Game{
 		return state;
 	}
 
+	/**Sets the GameState of the Game. Listeners are notified using stateChanged().*/
 	public void setState(GameState state) {
 		this.state = state;
 		//Informs all listeners that the state has changed
@@ -193,15 +219,15 @@ public class Game{
 		}
 	}
 
+	/**This method is called whenever an obstacle starts. All listeners are notified that this has happened.*/
 	public void obstacleStarted(Obstacle obstacle) {
-		// called whenever an obstacle starts, notifying all listeners that an obstacle has occured (handled by ... 
 		for (ObstacleListener listener : obstacleListeners) {
 			listener.started(obstacle);
 		}
 	}
 
+	/**This method is called whenever an obstacle end. All listeners are notified that this has happened.*/
 	private void obstacleEnded(Obstacle obstacle) {
-		// called whenever an obstacle ends, notifying all listeners that an obstacle has occured (handled by ... 
 		for (ObstacleListener listener : obstacleListeners) {
 			listener.ended(obstacle);
 		}
@@ -210,7 +236,8 @@ public class Game{
 	public void subscribeObstacleChanged(ObstacleListener obstacleListener) {
 		obstacleListeners.add(obstacleListener);
 	}
-
+	
+	/**This method causes one obstacle to happen at random, notifying the listeners.*/
 	private void calculateObstacles() {
 		//Choose where we get our obstacle based on whether the GameScreen is a recording
 		if(!GameScreen.instance.getClass().equals(RecordingScreen.class))
@@ -244,7 +271,7 @@ public class Game{
 		else
 		{
 			//We are in a recording and need to load our Obstacle from the playback
-			
+
 			RecordingScreen rec = (RecordingScreen)GameScreen.instance;
 			ObstacleEvent obstacleEvent = rec.eventPlayer.getNextObstacleEvent();
 			for(Tuple<Obstacle, Float> o : obstacleManager.getObstacles())
@@ -258,6 +285,7 @@ public class Game{
 		}
 	}
 
+	/**This method decreases the remaining duration of any remaining obstacles by 1 turn. If the duration has reached 0, the obstacle is removed and all listeners are notified.*/
 	private void decreaseObstacleTime() {
 		// decreases any active obstacles time left active by 1
 		ArrayList<Tuple<Obstacle, Float>> obstacles = obstacleManager.getObstacles();
@@ -288,31 +316,31 @@ public class Game{
 			instance = setupGame;
 		}
 	}
-	 
-	 //This method clones the current instance and stores it
-	 public static void storeLastTurn()
-	 {
-		 System.out.println("Stored");
-		 if(nextPreviousTurn != "")
-		 {
-			 previousTurn = nextPreviousTurn;
-		 }
-		 nextPreviousTurn = SaveManager.getSaveText();
-	 }
-	 
-	 //If we can, we step the game back a turn using this method
-	 public static void undoTurn()
-	 {
-		 if(previousTurn != "")
-		 {
-			 setInstance(SaveManager.loadFromText(previousTurn), true);
-			 previousTurn = "";
-		 }
-	 }
+
+	//This method clones the current instance and stores it
+	public static void storeLastTurn()
+	{
+		System.out.println("Stored");
+		if(nextPreviousTurn != "")
+		{
+			previousTurn = nextPreviousTurn;
+		}
+		nextPreviousTurn = SaveManager.getSaveText();
+	}
+
+	//If we can, we step the game back a turn using this method
+	public static void undoTurn()
+	{
+		if(previousTurn != "")
+		{
+			setInstance(SaveManager.loadFromText(previousTurn), true);
+			previousTurn = "";
+		}
+	}
 
 	public void notifyStarted() {
 		getObstacleManager().activateIdleObstacles();
 	}
-	 
-	 
+
+
 }
